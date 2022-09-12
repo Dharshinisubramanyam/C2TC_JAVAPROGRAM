cf0b5af1a9eceacaff35d4ecdee72bfcb4bfb1b6
HP <HP@DESKTOP-CBJH47J>
2022-09-12 18:40:45 +0530

    first commit

---
+++ javaprojectone/src/javaprojectone/Inheritance.java
@@ -0,0 +1,16 @@
+package javaprojectone;
+class withdraw
+{
+	int amt_withdraw=2000;
+	
+}
+
+public class Inheritance extends withdraw {
+int amt_left=5000;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Inheritance ob1= new  Inheritance();
+		 System.out.println("amountwithdraw"+ ob1.amt_withdraw);
+	}
+
+}
