fce779780ba3b40fb17016390516fc7fe986824c
HP <HP@DESKTOP-CBJH47J>
2022-09-15 16:43:32 +0530

    Signed-off-by: HP <HP@DESKTOP-CBJH47J> first commit

--- javaprojectone/src/javaprojectone/Assignment_temperature.java
+++ javaprojectone/src/javaprojectone/Assignment_temperature.java
@@ -1,10 +1,15 @@
 package javaprojectone;
-
+import java.util.Scanner;
 public class Assignment_temperature {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int temperature=sc.nextInt();;
+		int week=5;
+		for(int i=1;i<week;i++) {
+			temperature=temperature-2;
+			System.out.println(temperature);
 	}
-
+	}
 }
