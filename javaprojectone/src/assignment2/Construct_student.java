3ef18d8da8a5f520933ef0ad1556eb2ec1577267
HP <HP@DESKTOP-CBJH47J>
2022-09-19 10:28:33 +0530

    first commit

---
+++ javaprojectone/src/assignment2/Construct_student.java
@@ -0,0 +1,12 @@
+package assignment2;
+
+public class Construct_student {
+	Construct_student(){
+		System.out.println("Student object is created");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Construct_student xc=new Construct_student();
+	}
+
+}
